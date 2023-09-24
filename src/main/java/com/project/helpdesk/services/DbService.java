b8037d979c030aa080983ae7f6e230ec8871c28d
Marcus Martins <marcusmartins739@gmail.com>
2023-09-24 14:34:05 -0300

    Perfil de teste criado

---
+++ src/main/java/com/project/helpdesk/services/DbService.java
@@ -0,0 +1,41 @@
+package com.project.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.helpdesk.domain.Chamado;
+import com.project.helpdesk.domain.Cliente;
+import com.project.helpdesk.domain.Tecnico;
+import com.project.helpdesk.domain.enums.Perfil;
+import com.project.helpdesk.domain.enums.Prioridade;
+import com.project.helpdesk.domain.enums.Status;
+import com.project.helpdesk.repositories.ChamadoRepository;
+import com.project.helpdesk.repositories.ClienteRepository;
+import com.project.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DbService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+
+	public void instanciaDB() {
+		Tecnico tec1 = new Tecnico(null, "Validr Cezar", "63653230268", "valdir@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "70511744013", "torvalds@mail.com", "123");
+		
+		Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(chamado1));
+	}
+	
+}
